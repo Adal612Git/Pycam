482c838e9e38741951c3b909fb80ad60cbb2436b
Ricardo Adalberto Romero Medina <42826880+Adal612Git@users.noreply.github.com>
2025-07-25 09:46:25 -0600

    Import os for posture loader

--- PosturaZen/main.py
+++ PosturaZen/main.py
@@ -2,18 +2,16 @@
 
 import os
 
-from PosturaZen.calibracion.calibrador import Calibrador, PosturaBase
 from PosturaZen.deteccion.detector import Detector, cargar_postura
 
 BASE_PATH = os.path.join(os.path.dirname(__file__), "postura_base.json")
 
 
 def main() -> None:
-    if not os.path.exists(BASE_PATH):
-        calibrador = Calibrador()
-        postura_base = calibrador.calibrar()
-    else:
-        postura_base = cargar_postura(BASE_PATH)
+    """Ejecuta la detección de postura asegurando calibración válida."""
+
+    # Cargar la calibración o generar una de prueba si es necesario
+    postura_base = cargar_postura(BASE_PATH)
 
     no_molestar = os.environ.get("POSTURAZEN_SILENCIO", "0") == "1"
     detector = Detector(postura_base, no_molestar=no_molestar)
